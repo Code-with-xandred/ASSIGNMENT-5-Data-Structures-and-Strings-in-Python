23c01a45a0a18de0823f134a9159428c545fef90
Code-with-xandred <stak55795@gmail.com>
2025-02-27 23:41:58 +0530

    Task 2

---
+++ list_slicing.py
@@ -0,0 +1,11 @@
+# Task 2: Demonstrate List Slicing 
+
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+extracted_elements = original_list[:5]
+
+reversed_elements = extracted_elements[::-1]
+
+print("Original list:", original_list)
+print("Extracted first five elements:", extracted_elements)
+print("Reversed extracted elements:", reversed_elements)
